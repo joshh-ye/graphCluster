2cbf767b64c8681684de42a02e309b0de212de4a
Joshua Y <joshua.ye2005@gmail.com>
2025-06-08 23:30:57 -0700

    added functionality, csv to graph converter

--- streamlitLearn.py
+++ streamlitLearn.py
@@ -1,22 +1,58 @@
 import streamlit as st
 import numpy as np
 import time
+import streamlit as st
+import pandas as pd
 
-st.title("random graph generator")
+st.title("Graphing tools")
 
-if st.button("Start"):
-    coord = np.random.rand(1, 1)
-    chart = st.line_chart(coord)
+tab1, tab2 = st.tabs(["Random graph generator", "CSV to graph converter"])
 
-    with st.spinner("please wait...", show_time=True):
-        progressbar = st.sidebar.progress(0)
-        for i in range(1, 30):
-            new_coord = coord[-1] + np.random.rand(1, 1) * 2 - 1
-            chart.add_rows(new_coord)
-            coord = new_coord
+with tab1:
+    st.title("random graph generator")
 
-            progressbar.progress(i/30)
-            time.sleep(0.1)
+    if st.button("Start"):
+        coord = np.random.rand(1, 1)
+        chart = st.line_chart(coord)
 
-    progressbar.empty()
-    st.button("Rerun")
+        with st.spinner("please wait...", show_time=True):
+            progressbar = st.sidebar.progress(0)
+            for i in range(1, 30):
+                new_coord = coord[-1] + np.random.rand(1, 1) * 2 - 1
+                chart.add_rows(new_coord)
+                coord = new_coord
+
+                progressbar.progress(i/30)
+                time.sleep(0.1)
+
+        progressbar.empty()
+
+
+with tab2:
+    st.title("CSV to graph converter")
+
+    file = st.file_uploader("choose CSV file", type="csv")
+
+    if file is not None:
+        df = pd.read_csv(file)
+
+        st.subheader("Data preview")
+        st.write(df.head())
+
+        st.write(df.describe())
+
+        st.subheader("filter data")
+        filteredColumns = st.selectbox("Choose column to filter", ['--choose column--'] + list(df.columns))
+
+        if filteredColumns != '--choose column--':
+            unique = df[filteredColumns].unique()
+
+            value = st.selectbox("Choose value from filtered column", unique)
+            final = df[df[filteredColumns] == value]
+            st.write(final)
+
+            x_value = st.selectbox("choose x values", df.columns)
+            y_value = st.selectbox("choose y values", df.columns)
+
+            if st.button("Generate plot"):
+                st.line_chart(final.set_index(x_value)[y_value])
