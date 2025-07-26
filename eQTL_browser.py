31fbfd7d1646f0d87b6b06812a52f6128ee430b7
Joshua Ye <josh@Joshuas-MacBook-Pro.local>
2025-07-26 12:03:33 -0700

    ebi eQTL API switched to more stable v2 for faster loading times

--- eQTL_browser.py
+++ eQTL_browser.py
@@ -2,50 +2,61 @@ import pandas as pd
 import plotly.express as px
 import requests
 import streamlit as st
-
 st.title("eQTL Browser")
-st.sidebar.title("query paramaters")
+st.sidebar.title("Query Parameters")
 
 # Sidebar: Input options
-entry_limit = st.sidebar.slider("Choose number of entries for table", 0, 1000, 50)
-search_option = st.sidebar.selectbox("Which search method would you like to use?", ("gene_id", "RSID", "gene name"), )
-
-id = ""
-if search_option == "gene_id":
-    id = st.sidebar.text_input("Enter Ensembl Gene ID", value="ENSG00000215014")
-else:
-    id = st.sidebar.text_input("Enter RSID", value="rs200141179")
-
-quant_methods = ["ge", "exon", "microarray", "tx", "txrev"]
-selected_method = st.sidebar.selectbox("Choose quantification method", quant_methods, index=0)
+entry_limit = st.sidebar.slider("Choose number of entries for table", 0, 1000, 500)
 pval_thresh = st.sidebar.slider("P-value threshold", 0.0, 1.0, 0.05, 0.001)
 
 # Query eQTL API
 query_params = {
     "size": entry_limit,
-    "p_upper": pval_thresh,
-    "quant_method": selected_method
 }
+
+search_option = st.sidebar.selectbox("Which search method would you like to use?", ("gene_id", "RSID", "gene name"), ) #gene name is upcoming feature
+if search_option == "RSID":
+    rsid = st.sidebar.text_input("Enter RSID", value="rs200141179")
+    query_params["rsid"] = rsid
+else:
+    gene_id = st.sidebar.text_input("Enter Ensembl Gene ID", value="ENSG00000188157")
+    query_params["gene_id"] = gene_id
+
 
 
 @st.cache_data
-def fetch_data(search_option, id, query_params):
-    if search_option == "gene_id":
-        api_response = requests.get(f'https://www.ebi.ac.uk/eqtl/api/genes/{id}/associations',
-                                    params=query_params).json()
-    elif search_option == "RSID":
-        api_response = requests.get(f'https://www.ebi.ac.uk/eqtl/api/associations/{id}',
-                                    params=query_params).json()
-    else:
-        api_response = 'https://www.ebi.ac.uk/eqtl/api/associations'
-    return api_response
+def fetch_data(query_params):
+    try:
+        response = requests.get('https://www.ebi.ac.uk/eqtl/api/v2/datasets/QTD000370/associations?start=0&size=20',
+                                params=query_params)
+        response.raise_for_status()
+        try:
+            data = response.json()
+        except ValueError:
+            st.error(f"Failed to parse JSON. Response text: {response.text}")
+            return None
+        return data
+    except requests.exceptions.HTTPError as e:
+        st.error(f"HTTP error {response.status_code}: {response.text}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request error: {e}")
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
+    return None
 
 
 try:
     with st.spinner("Loading data", show_time=True):
-        api_response = fetch_data(search_option, id, query_params)
+        api_response = fetch_data(query_params)
 
-    association_df = pd.DataFrame(list(api_response['_embedded']['associations'].values()))
+
+    st.title("All data")
+    df = pd.DataFrame(api_response)
+    st.write(df)
+
+    #p_value filter
+    st.title("Filtered p-value data")
+    association_df = df[df['pvalue'] <= pval_thresh]
     st.write(association_df)
 
     # Compute genomic position range for slider
